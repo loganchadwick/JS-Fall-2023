54c073a0ec0a6973fe0b370a4f7a75bfab7b589a
Logan Chadwick <loganchadwick@Logans-MacBook-Pro-3.local>
2023-10-13 12:05:16 -0700

    Assignment 1

---
+++ Modules/Class1/VariablesAssignment.js
@@ -0,0 +1,17 @@
+let firstName = 'Logan';
+console.log(firstName);
+
+let age = 25;
+console.log(age);
+
+let isStudent = true;
+console.log(isStudent);
+
+let favoriteColors = ['blue', 'green', 'yellow'];
+console.log(favoriteColors);
+
+let person = {
+    firstName : 'Logan',
+    age : 25, 
+    isStudent : true};
+console.log(person);
