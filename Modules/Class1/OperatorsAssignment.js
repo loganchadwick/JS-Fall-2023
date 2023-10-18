6c63557fd05412a11e3c2150a399c346939c9004
Logan Chadwick <loganchadwick@Logans-MacBook-Pro-3.local>
2023-10-18 10:34:00 -0700

    Assignment

---
+++ Modules/Class1/OperatorsAssignment.js
@@ -0,0 +1,40 @@
+let num1 = 1;
+let num2 = 2;
+let product = num1 * num2;
+console.log(product);
+
+let radius = 2;
+let area = 3.14159 * radius ** 2;
+console.log(area);
+
+let count = 5;
+count += 2;
+console.log(count);
+
+let price = 10;
+price *= 2.5;
+console.log(price);
+
+let age1 = 25;
+let age2 = 28;
+console.log(num1 > num2);
+
+let varName = "Steve";
+console.log(varName != "John Doe");
+
+let isMember = true;
+let hasCoupon = false;
+console.log(isMember||hasCoupon);
+
+let num = 5;
+console.log(!(num = 0));
+
+let age = 25;
+let votingAge = (age >= 18) ? "Person is eligible to vote" : 
+"Person is not eligible to vote";
+console.log(votingAge);
+
+let price1 = 9.99;
+let onSale = (price1 <= 15) ? "Item is on sale" : 
+"Item is not on sale";
+console.log(onSale);
